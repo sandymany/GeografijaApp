b6f5ac53eb6eb7a2784a0e0b05ae1bb0ce4f9c21
sandymany <crnkovic.let@gmail.com>
2019-10-09 20:18:36 +0200

    added: passcode check and new activity

---
+++ app/src/main/java/com/leticija/treeapp/activities/MainActivity.java
@@ -0,0 +1,61 @@
+package com.leticija.treeapp.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.leticija.treeapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText passcodeField;
+    TextView message;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        passcodeField = findViewById(R.id.passcode_editText);
+        message = findViewById(R.id.passcode_message);
+
+        Button submitButton = findViewById(R.id.button_submit);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                goToNewActivity();
+            }
+        });
+
+        passcodeField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                    goToNewActivity();
+                }
+                return false;
+            }
+        });
+
+    }
+
+    private void goToNewActivity () {
+        if (passcodeField.getText().toString().equals(getString(R.string.passcode))) {
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(intent);
+        }
+        else {
+            message.setText("Kriva lozinka");
+            message.setVisibility(View.VISIBLE);
+        }
+    }
+}
