3aa279fd038b9f3760afc03436c7795e398fc3cb
sandymany <crnkovic.let@gmail.com>
2019-10-12 16:19:10 +0200

    added: google placePicker API (turn off internet)

--- app/src/main/java/com/leticija/treeapp/activities/AddTreeActivity.java
+++ app/src/main/java/com/leticija/treeapp/activities/AddTreeActivity.java
@@ -1,16 +1,67 @@
 package com.leticija.treeapp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.leticija.treeapp.R;
 
 
 
 public class AddTreeActivity  extends AppCompatActivity {
 
+    Button button;
+    TextView textView;
+    int PLACE_PICKER_REQUEST=1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_tree_activity);
+
+        button = findViewById(R.id.bt_picker);
+        textView = findViewById(R.id.text_view);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                try {
+                    startActivityForResult(builder.build(AddTreeActivity.this),PLACE_PICKER_REQUEST);
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode==PLACE_PICKER_REQUEST) {
+            if (resultCode==RESULT_OK) {
+                Place place = PlacePicker.getPlace(data,this);
+                StringBuilder stringBuilder = new StringBuilder();
+                String latitude = String.valueOf(place.getLatLng().latitude);
+                String longitude = String.valueOf(place.getLatLng().longitude);
+                stringBuilder.append("LATITUDE: ");
+                stringBuilder.append(latitude);
+                stringBuilder.append(" LONGITUDE: ");
+                stringBuilder.append(longitude);
+
+                textView.setText(stringBuilder.toString());
+
+            }
+        }
     }
 }
